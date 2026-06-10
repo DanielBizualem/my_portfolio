bd78c5177e7ade19d7ab2a92e3c2acf10311221b
Daniel Bizualem <danishow375@gmail.com>
2026-06-10 23:33:11 +0300

    under deployment

--- portfolio/common/summeryApi.js
+++ portfolio/common/summeryApi.js
@@ -1,4 +1,7 @@
-export const baseURL = 'http://localhost:5000';
+export const baseURL = typeof window !== "undefined" && 
+  (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
+    ? "http://localhost:5000" 
+    : "https://my-portfolio-ugr9.onrender.com";
 
 const summeryApi = {
     hire:{
